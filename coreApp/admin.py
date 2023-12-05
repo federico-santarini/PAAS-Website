097a27d805e01b5bad8a48a5824364936bfc86bb
federico-santarini <federico.santarini@gmail.com>
2023-12-05 23:18:22 +0100

    admin interface

--- coreApp/admin.py
+++ coreApp/admin.py
@@ -3,16 +3,53 @@ from .models import Glyph, SelectedImage
 from django.contrib.sessions.models import Session
 
 class GlyphAdmin(admin.ModelAdmin):
+    list_filter = (
+        'approvato',
+        'variante',
+        'categoria_semantica',
+        'funzione_grammaticale',
+        'figura_retorica',
+        'presenza_umana'
+
+
+
+    )
+    list_display = (
+                    'approvato',
+                    'id',
+                    'parola',
+                    'variante',
+                    'id_variante',                    
+                    'parola_ENG',
+                    'categoria_semantica',
+                    'funzione_grammaticale',
+                    'figura_retorica',
+                    'presenza_umana'
+                    )
+    
+    list_editable = (
+                    'categoria_semantica',
+                    'funzione_grammaticale',
+                    'figura_retorica',
+                    'presenza_umana',
+                    'parola_ENG',
+
+
+                    )
+    list_display_links = (
+                    'id', 
+                    )
+    
+    readonly_fields = ('created_at','modified_at')
+    
     fieldsets = [
         ("Status",{
-            'fields':('approvato',),
+            'fields':('approvato','created_at','modified_at'),
         }),
 
         ('Basic Informations', {
             'fields': (('id','id_variante','variante'),
-                       ('parola',
-                       'parola_ENG')
-                       
+                       ('parola', 'parola_ENG')
                        ),
         }),
 
